d0e4bc735164977acd1f4c4d514178d82882cbd5
Thalles Freitas <thalles.freitas@zup.com.br>
2022-05-25 11:56:08 -0300

    add prototype and singleton
    Signed-off-by: Thalles Freitas <thalles.freitas@zup.com.br>

---
+++ src/main/java/org/example/prototype/Circle.java
@@ -0,0 +1,15 @@
+package org.example.prototype;
+
+public class Circle extends Shape {
+
+    public Circle(Circle circle) {
+        super();
+        this.weight = circle.weight;
+        this.widht = circle.widht;
+    }
+
+    @Override
+    public Shape clone() {
+        return new Circle(this);
+    }
+}
